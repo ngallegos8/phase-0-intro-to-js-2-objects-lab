066faaa691a50d957d29d17e404d1ed0c51ae091
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-13 14:40:33 -0700

    7 passing tests, function templates instead of specific arguments

--- index.js
+++ index.js
@@ -1,36 +1,145 @@
 const employee = {
-    name: "Sam",
-    streetAddress: "11 Broadway",
+    name: "Link",
+    streetAddress: "1 Tarrey Town Road"
 }
 
-function updateEmployeeWithKeyAndValue(employee, streetAddress, updatedAddress) {
-    const newAddress = {...employee};
-
-    newAddress[streetAddress] = "11 Broadway"
-
-    return newAddress;
-}
-
-const newAddress = updateEmployeeWithKeyAndValue(
-    streetAddress,
-    "11 Broadway",
-    "12 Broadway"
-);
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, updatedAddress) {
-    employee[streetAddress] = "12 Broadway";
-    return employee;
-}
-
-function deleteFromEmployeeByKey(employee, streetAddress, updatedAddress) {
+//funtion to non-destructively change key and value (using spread operator)
+function updateEmployeeWithKeyAndValue (object, key, value) {
     return {
-        ...employee,
-        [streetAddress]: updatedAddress,
-    };
+        ...object,
+        [key]: value
+    }
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, streetAddress, updatedAddress) {
-    delete employee[streetAddress];
-    return employee;
-    };
+//executing non-destructive function for specific values of 'Sam' & '11 Broadway'
+
+const employeeOne = updateEmployeeWithKeyAndValue(
+    employee,
+    "Sam",
+    "11 Broadway"
+)
+
+//destructively mutate object
+function destructivelyUpdateEmployeeWithKeyAndValue (object, key, value){
+    object[key] = value
+    return object;
+
+    // return {
+    //     object,
+    //     [key]: value
+    // }
+}
+
+// function deleteFromEmployeeByKey(object, key, value){
+//     return{
+//         ...object,
+//         [key]: value
+
+//         delete 
+//     }
+// }
+
+function deleteFromEmployeeByKey(object, key, value){
+    return {
+        ...object,
+        [key]: value,
+    }
+
+    delete [key]
+}
+
+function destructivelyDeleteFromEmployeeByKey (object, key){
+    // object[key] = value;
+    delete object[key];
+    return object
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name: "Sam",
+//     streetAddress: "11 Broadway",
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, streetAddress, updatedAddress) {
+//     const newAddress = {...employee};
+
+//     newAddress[streetAddress] = "11 Broadway"
+
+//     return newAddress;
+// }
+
+// const newAddress = updateEmployeeWithKeyAndValue(
+//     streetAddress,
+//     "11 Broadway",
+//     "12 Broadway"
+// );
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, updatedAddress) {
+//     employee[streetAddress] = "12 Broadway";
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, streetAddress, updatedAddress) {
+//     return {
+//         ...employee,
+//         [streetAddress]: updatedAddress,
+//     };
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, streetAddress, updatedAddress) {
+//     delete employee[streetAddress];
+//     return employee;
+//     };
 
